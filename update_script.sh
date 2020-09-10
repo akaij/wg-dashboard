e9ee4989eb3c5da54d4f353e84133d04bc308372
akay <akaij@users.noreply.github.com>
2020-09-10 09:21:18 +0300

    Update update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$EUID" -ne 0 ]]; then
 	echo "Sorry, this script must be ran as root"
 	echo "Maybe try this:"
-	echo "curl https://raw.githubusercontent.com/wg-dashboard/wg-dashboard/master/update_script.sh | sudo bash"
+	echo "curl https://raw.githubusercontent.com/akaij/wg-dashboard/master/update_script.sh | sudo bash"
 	exit
 fi
 
@@ -14,14 +14,14 @@ cd /opt
 if [ ! -d wg-dashboard ]; then
 	echo "You do not appear to have wg-dashboard installed."
 	echo "Try running this instead:"
-	echo "curl https://raw.githubusercontent.com/wg-dashboard/wg-dashboard/master/install_script.sh | sudo bash"
+	echo "curl https://raw.githubusercontent.com/akaij/wg-dashboard/master/install_script.sh | sudo bash"
 	exit
 fi
 
 # backup existing config
 cp wg-dashboard/server_config.json wg-dashboard/server_config.bak
 # download latest release
-curl -L https://github.com/$(wget https://github.com/wg-dashboard/wg-dashboard/releases/latest -O - | egrep '/.*/.*/.*tar.gz' -o) --output wg-dashboard.tar.gz
+curl -L https://github.com/$(wget https://github.com/akaij/wg-dashboard/releases/latest -O - | egrep '/.*/.*/.*tar.gz' -o) --output wg-dashboard.tar.gz
 # untar latest release into existing install
 tar -xzf wg-dashboard.tar.gz --strip-components=1 -C wg-dashboard
 # delete unpacked .tar.gz
